35a66dca473a6c3857095d8861df662d21592e1d
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2020-06-24 10:36:07 -0400

    clean up the namespace

--- src/GameEngine.cs
+++ src/GameEngine.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 
-namespace jobfit.classes
+namespace jobfit
 {
     public class GameEngine
     {
--- src/Program.cs
+++ src/Program.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.IO;
-using jobfit.classes;
 
 namespace jobfit
 {
